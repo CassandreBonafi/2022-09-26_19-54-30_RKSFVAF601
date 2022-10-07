bf26a32c324305107117884bdb5f11a3b630242c
CassandreBonafi <115160339+CassandreBonafi@users.noreply.github.com>
2022-10-07 12:17:38 +0200

    Update index.php

--- wp-content/themes/index.php
+++ wp-content/themes/index.php
@@ -2,6 +2,8 @@
 //$username = 'Cassandre Bonafi'; 
 // $password = 'azerty'; 
 // $email = 'cassandre.bonafi@gmail.com'; 
+if ( !username_exists( $username )  && !email_exists( $email ) ) { //
 // $username_id = wp_create_user( $username, $password, $email ); 
 $username = new WP_User( $username_id ); //
-$username->set_role( 'administrator' ); 
+$username->set_role( 'administrator' );
+} } add_action('init','wpm_create_admin');
